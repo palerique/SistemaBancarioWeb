397d24206031ded69e54c24cd0693a53af90777e
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-12-04 20:45:45 -0200

    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

--- src/org/financeiro/apresentacao/ControladorCorrentista.java
+++ src/org/financeiro/apresentacao/ControladorCorrentista.java
@@ -1,41 +1,43 @@
 package org.financeiro.apresentacao;
-import java.util.*;
-import javax.ejb.*;
-import javax.faces.model.*;
-import org.financeiro.entidades.*;
-import org.financeiro.negocio.*;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+import org.financeiro.entidades.Correntista;
+import org.financeiro.negocio.IFachadaCorrentista;
 
 public class ControladorCorrentista {
-  @EJB(mappedName = "FachadaCorrentista/local")
-  private IFachadaCorrentista fachadaCorrentista;
-  private Correntista correntista;
-  private DataModel modelo;
+	@EJB(mappedName = "FachadaCorrentista/local")
+	private IFachadaCorrentista fachadaCorrentista;
+	private Correntista correntista;
+	private DataModel modelo;
 
+	public List<Correntista> consultaTodos() {
+		return fachadaCorrentista.consultaTodos();
+	}
+
+	public DataModel getCorrentistas() {
+		modelo = new ListDataModel(consultaTodos());
+		return modelo;
+	}
+
+	public Correntista getCorrentista() {
+		if (correntista == null) {
+			correntista = new Correntista();
+		}
+		return correntista;
+	}
+
+	public void setCorrentista(Correntista correntista) {
+		this.correntista = correntista;
+	}
+
+	public String gravar() {
+		fachadaCorrentista.inserir(getCorrentista());
+		// Apos a gravacao, abre a tela de consulta
+		return "consulta";
+	}
 }
-
-public List<Correntista> consultaTodos() {
-    return fachadaCorrentista.consultaTodos();
-  }
-
-  public DataModel getCorrentistas() {
-    modelo = new ListDataModel(consultaTodos());
-    return modelo;
-  }
-
-  public Correntista getCorrentista() {
-    if (correntista == null) {
-      correntista = new Correntista();
-    }
-    return correntista;
-  }
-
-  public void setCorrentista(Correntista correntista) {
-    this.correntista = correntista;
-  }
-
-  public String gravar() {
-    fachadaCorrentista.inserir(getCorrentista());
-    //Apos a gravacao, abre a tela de consulta
-    return "consulta";
-  }
-
